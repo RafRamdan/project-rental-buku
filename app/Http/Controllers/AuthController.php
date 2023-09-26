26baf36ad970b44aa307b1218e077dc73ec22965
RafRamdan <rafiramdan2310@gmail.com>
2023-09-26 16:52:56 +0700

    Menambahkan login dan authentication middleware role id

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login()
     {
         return view('login');
-    } 
+    }
+    
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function authenticating(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // $request->session()->regenerate();
+            // return redirect()->intended('dashboard');
+            if (Auth::user()->status != 'active'){
+                Session::flash('status', 'failed');
+                Session::flash('message', 'Your account is not active yet,please contact admin!');
+                return redirect('/login');
+            }
+
+            if(Auth::user()->role_id == 1){
+                return redirect('dashboard');
+            }
+
+            if(Auth::user()->role_id == 2){
+                return redirect('profile');
+            }
+            // $request->session()->regenerate();
+            // if
+            // return redirect();
+        }
+
+        Session::flash('status', 'failed');
+        Session::flash('message', 'Login infalid');
+        return redirect('/login');
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');   
+    }
 }
+
+
